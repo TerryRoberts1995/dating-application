7738f69e9bc41c498b9214dfb9d9651ecf90afc1
tbytheway <tdbytheway@gmail.com>
2020-07-31 11:24:43 -0600

    new sign-in page

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import Cookie from "js-cookie";
+import { navigate } from "hookrouter";
 import '../../styles/Nav.css'
 import tree from '../../images/tree.jpg'
 
+const username = Cookie.get("username");
 export default function Nav() {
+    
+    const handleLogout = () => {
+        Cookie.remove("username");
+        Cookie.remove("password");
+         navigate("/");
+    };
+
     return (
         <div className="navbar-wrapper">
             <div className="navbar-logo">
@@ -16,6 +26,12 @@ export default function Nav() {
                 <NavLink to='/'>Sign In</NavLink>
                 <NavLink to='/sign-up'>Sign Up</NavLink>
             </div>
+ 
+            <div>
+                <p>Hello, {username}</p>
+                <input type="submit" onClick={handleLogout} value="Logout" />
+            </div>
+
 
             {/* ADD USERNAME IN DIV */}
         </div >
